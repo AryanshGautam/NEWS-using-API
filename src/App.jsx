b4aefd2d360de793a3de960a71e926997fd5b0d7
AryanshGautam <159770996+AryanshGautam@users.noreply.github.com>
2024-04-25 14:15:57 +0530

    Uploaded a new site made using the API from NEWS API

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import NewsBoard from './components/NewsBoard'
+
+function App() {
+  const [category, setCategory] = useState("general");
+
+  return (
+    <>
+      <div>
+        <Navbar setCategory={setCategory}/>
+        <NewsBoard category={category}/>
+      </div>
+    </>
+  )
+}
+
+export default App
