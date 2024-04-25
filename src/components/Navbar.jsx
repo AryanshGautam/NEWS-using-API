a49312ded1659470db03ae820e66872394b8a0e1
AryanshGautam <159770996+AryanshGautam@users.noreply.github.com>
2024-04-25 14:20:06 +0530

    Changed the NEWS Page name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar({setCategory}) {
     <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"> <span className='badge bg-light text-dark fs-4'>Sushank NEWS</span> </a>
+    <a className="navbar-brand" href="#"> <span className='badge bg-light text-dark fs-4'>ALL NEWS</span> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
